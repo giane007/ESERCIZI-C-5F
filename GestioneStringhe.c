5abad10fcf4410f17b571a4fc762d9412211a668
IL GIANE <150336285+giane007@users.noreply.github.com>
2025-09-23 08:58:56 +0200

    Update GestioneStringhe.c

--- GestioneStringhe.c
+++ GestioneStringhe.c
@@ -1,3 +1,17 @@
+/*
+Scrivere un programma in C che dopo aver inserito una stringa a piacere determini e o verifichi:
+1) Che contenga solo lettere;
+2) Il conteggio di una lettera (se presente) scelta dall’utente;
+3) Crei 2 ulteriori stringhe che contengano 1 le lettere di posizione pari; la 2° le lettere di posizione
+dispari;
+4) Verifichi se contiene doppie.
+Inserita una 2° stringa determinare:
+1) Quale delle 2 è più lunga e più corta;
+2) Quali sono le lettere contenute in ambedue le stringhe;
+3) Quale delle 2 stringhe contiene più vocali;
+4) Quale delle 2 stringhe contiene più consonanti;
+*/
+
 #include <stdio.h>
 #include <string.h>
 
